2facef28f4ba9a5ecc9bb614a6f198aabf0742b9
Priyatosh Das <priyatoshdas005@gmail.com>
2023-02-17 07:29:34 +0530

    xxxxxx

--- lib/Newattendancebox.dart
+++ lib/Newattendancebox.dart
@@ -18,83 +18,85 @@ class DialogBox extends StatelessWidget {
     return AlertDialog(
       backgroundColor: Gcolors.neutralColor1000,
       content: Container(
-        height: 200,
-        width: 300,
-        child: Column(
-          children: [
-            Text(
-              'Add Subjects',
-              style: GoogleFonts.inter(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Gcolors.primaryColor050),
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 150.0),
-              child: Text(
-                'Subject Name:',
+        height: 250,
+        width: 320,
+        child: Expanded(
+          child: Column(
+            children: [
+              Text(
+                'Add Subjects',
                 style: GoogleFonts.inter(
-                    color: Gcolors.primaryColor050, fontSize: 16),
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Gcolors.primaryColor050),
               ),
-            ),
-            SizedBox(
-              height: 12,
-            ),
-            TextField(
-              controller: subject,
-              style: TextStyle(color: Gcolors.primaryColor050),
-              decoration: InputDecoration(
-                //color: Gcolors.neutralColor900,
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
-                hintText: 'e.g Mathematics',
-                hintStyle: TextStyle(
-                  color: Gcolors.neutralColor400,
-                ),
-                //labelStyle: TextStyle(color: Gcolors.primaryColor050),
-                // fillColor: Gcolors.primaryColor050
-                // borderRadius: BorderRadius.circular(20)
+              SizedBox(
+                height: 16,
               ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            //Text('Initial Presents:',style: GoogleFonts.inter(fontSize: 16,color: Gcolors.primaryColor050),)
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  child: ElevatedButton(
-                      onPressed: onCancel,
-                      style: ElevatedButton.styleFrom(
-                          side: BorderSide(width: 2, color: Colors.red),
-                          backgroundColor: Gcolors.neutralColor1000),
-                      child: Text(
-                        'CANCEL',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Gcolors.primaryColor050),
-                      )),
+              Padding(
+                padding: const EdgeInsets.only(right: 10.0),
+                child: Text(
+                  'Subject Name:',
+                  style: GoogleFonts.inter(
+                      color: Gcolors.primaryColor050, fontSize: 15),
                 ),
-                SizedBox(
-                  width: 16,
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              TextField(
+                controller: subject,
+                style: TextStyle(color: Gcolors.primaryColor050),
+                decoration: InputDecoration(
+                  //color: Gcolors.neutralColor900,
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  hintText: 'e.g Mathematics',
+                  hintStyle: TextStyle(
+                    color: Gcolors.neutralColor400,
+                  ),
+                  //labelStyle: TextStyle(color: Gcolors.primaryColor050),
+                  // fillColor: Gcolors.primaryColor050
+                  // borderRadius: BorderRadius.circular(20)
                 ),
-                Container(
-                  
-                    width: 90,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              //Text('Initial Presents:',style: GoogleFonts.inter(fontSize: 16,color: Gcolors.primaryColor050),)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
                     child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(backgroundColor: Gcolors.primaryColor400),
-                        onPressed: onSave,
+                        onPressed: onCancel,
+                        style: ElevatedButton.styleFrom(
+                            side: BorderSide(width: 2, color: Colors.red),
+                            backgroundColor: Gcolors.neutralColor1000),
                         child: Text(
-                          'SAVE',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ))),
-              ],
-            )
-          ],
+                          'CANCEL',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Gcolors.primaryColor050),
+                        )),
+                  ),
+                  SizedBox(
+                    width: 16,
+                  ),
+                  Container(
+                      width: 90,
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Gcolors.primaryColor400),
+                          onPressed: onSave,
+                          child: Text(
+                            'SAVE',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ))),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
